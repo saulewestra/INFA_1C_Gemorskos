b79440f70bff790217015b50d5c4546a61e80fae
Saule Westra <saule.westra@student.nhlstenden.com>
2022-12-13 20:31:15 +0100

    sql File is aangemaakt voor opzet 'Gemorskos' table.

---
+++ database/Gemorskos.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS `Gemorskos`
+DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `Gemorskos`
+
+CREATE TABLE `Medewerker`(
+
+)
+
+CREATE TABLE `Werk_Title`(
+
+)
+
+CREATE TABLE `Evenement`(
+
+)
+
+CREATE TABLE `Evenement_Detail`(
+
+)
