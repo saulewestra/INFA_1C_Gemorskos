d11d92dd67e5872cfebafeab1df4c45a70179c5b
Saule Westra <saule.westra@student.nhlstenden.com>
2022-12-20 17:19:42 +0100

    Feature/Database is aangemaakt. Bestanden zijn toegevoegd en aangepast.

--- database/Gemorskos.sql
+++ database/Gemorskos.sql
@@ -6,55 +6,48 @@ USE `Gemorskos`
 
 -- 1. Tabel voor medewerkers
 CREATE TABLE `Medewerkers`(
-`medewerker_id` INT AUTO_INCREMENT NOT NULL;
-`werk_functie_id` INT NOT NULL;
-`voornaam` VARCHAR(25) NOT NULL;
-`achternaam` VARCHAR(25) NOT NULL;
-`email` VARCHAR(55) UNIQUE NOT NULL;
-`telefoonnummer` VARCHAR(10) UNIQUE NOT NULL;
+`medewerker_id` INT AUTO_INCREMENT NOT NULL,
+`werk_functie_id` INT NOT NULL,
+`voornaam` VARCHAR(25) NOT NULL,
+`achternaam` VARCHAR(25) NOT NULL,
+`email` VARCHAR(55) UNIQUE NOT NULL,
+`telefoonnummer` VARCHAR(10) UNIQUE NOT NULL,
+`wachtwoord` VARCHAR(60) NOT NULL,
 PRIMARY KEY(`medewerker_id`),
-FOREIGN KEY(`werk_functie_id`) REFERENCES `Werk_Functie`(`werk_functie_id`) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY(`werk_functie_id`) REFERENCES `Werk_Functie`(`werk_functie_id`) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
 -- 2. Tabel voor de soorten werk functies
 CREATE TABLE `Werk_Functie`(
-`werk_functie_id` INT AUTO_INCREMENT NOT NULL;
-`functie_naam` VARCHAR(14) NOT NULL; CREATE DATABASE IF NOT EXISTS `Gemorskos`
+`werk_functie_id` INT AUTO_INCREMENT NOT NULL,
+`functie_naam` VARCHAR(14) NOT NULL,
 PRIMARY KEY(`werk_functie_id`)
 );
 
--- 3. Tabel voor het inloggen van de medewerker
-CREATE TABLE `Inlog`(
-`inlog_id` INT AUTO_INCREMENT NOT NULL;
-`username` VARCHAR(55) UNIQUE NOT NULL;
-`wachtwoord` VARCHAR(60) NOT NULL;
-`medewerker_id` INT NOT NULL;
-PRIMARY KEY(`inlog_id`),
-FOREIGN KEY(`medewerker_id`) REFERENCES `Medewerkers`(`medewerker_id`) ON UPDATE CASCADE ON DELETE NO ACTION
-);
-
--- 4. Tabel voor de evenementen die te zien zijn
+-- 3. Tabel voor de evenementen die te zien zijn
 CREATE TABLE `Evenement`(
-`evenement_id` INT AUTO_INCREMENT NOT NULL;
-`evenement_naam` VARCHAR(40) NOT NULL;
-`beschrijving` TEXT NOT NULL;
-`dag` DATE
-`tijd` TIME
-`straatnaam` VARCHAR(26) NOT NULL;
-`stad` VARCHAR(40) NOT NULL;
-`postcode` VARCHAR(6) NOT NULL;
+`evenement_id` INT AUTO_INCREMENT NOT NULL,
+`evenement_naam` VARCHAR(40) NOT NULL,
+`beschrijving` TEXT NOT NULL,
+`dag` DATE,
+`tijd` TIME,
+`straatnaam` VARCHAR(26) NOT NULL,
+`stad` VARCHAR(40) NOT NULL,
+`postcode` VARCHAR(6) NOT NULL,
 PRIMARY KEY(`evenement_id`)
 );
 
--- 5. Tabel voor informatie van de geclaimde events
+-- 4. Tabel voor informatie van de geclaimde events
 CREATE TABLE `Evenement_Detail`(
-`medewerker_id` INT NOT NULL;
-`evenement_id`INT NOT NULL;
-FOREIGN KEY(`medewerker_id`) REFERENCES `Medewerker`(`medewerker_id`) ON UPDATE CASCADE ON DELETE NO ACTION,
+`medewerker1_id` INT NOT NULL,
+`medewerker2_id` INT NOT NULL,
+`evenement_id`INT NOT NULL,
+FOREIGN KEY(`medewerker1_id`) REFERENCES `Medewerker`(`medewerker_id`) ON UPDATE CASCADE ON DELETE NO ACTION,
+FOREIGN KEY(`medewerker2_id`) REFERENCES `Medewerker`(`medewerker_id`) ON UPDATE CASCADE ON DELETE NO ACTION,
 FOREIGN KEY(`evenement_id`) REFERENCES `Evenement`(`evenement_id`) ON UPDATE CASCADE ON DELETE NO ACTION
 );
 
--- 6. Tabel voor Bestanden
+-- 5. Tabel voor Bestanden
 CREATE TABLE `Bestand`(
 `bestand_id` INT AUTO_INCREMENT NOT NULL;
 `medewerker_id` INT NOT NULL;
