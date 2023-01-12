31bdf0bcff5b13eb2c30a88c6aa22c9a1a7470ac
Saule Westra <saule.westra@student.nhlstenden.com>
2023-01-12 16:10:46 +0100

    Register wordt toegevoegd

---
+++ app/public/register/Register.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../style/style.css">
+        <title>Register</title>
+    </head>
+    <body>
+        <div id ="wrapper">
+            <header id="header">
+                <div id="logo">
+                    <h1>Gemorskos</h1>
+                    <h3>Wij maken kranten</h3>
+                </div>
+                <!--Hier moet de path komen om de dropdown te zien-->
+                <a href="#"><img id="profile" src="../img/profilepic.png" alt="Profile"></a>
+            </header>
+            <div id="background"></div>
+        </div>
+    </body>
+</html>
